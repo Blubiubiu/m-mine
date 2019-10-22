b7c2e142376139b0952fe8b82bd3bffb9095d569
lisiyuan <lisiyuan6@100tal.com>
2019-10-22 16:35:28 +0800

    v1.0.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div>啥都没有呢还</div>
-    <a href="/resource">pc</a>
+    <a href="/resource">pc1</a>
     <div style="position: fixed; bottom:0; width: 100%; text-align: center;">网站备案：京ICP备19031081号</div>
   </div>
 </template>
